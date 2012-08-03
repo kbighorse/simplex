17e0ac84031dc75a57f048c4e080eb7b2f2dee40
Kimball Bighorse <kbighorse@yahoo.com>
2012-08-03 16:25:24 -0700

    fixed bad js call

--- app/assets/javascripts/bootswatch.js
+++ app/assets/javascripts/bootswatch.js
@@ -1,7 +1,7 @@
-$('#main-menu').load('http://bootswatch.com/js/snippets/main-menu.html', function(){
+$('#main-menu').load('main-menu.html', function(){
 	if($('body').hasClass('preview')){
 		$('#main-menu-left').append('<li class="dropdown" id="preview-menu"></li>');
-		$('#preview-menu').load('http://bootswatch.com/js/snippets/preview-menu.html');
+		$('#preview-menu').load('preview-menu.html');
 	}
 
 	$('a[rel=tooltip]').tooltip({
